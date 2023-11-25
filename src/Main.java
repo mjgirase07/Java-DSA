3b8543c6d4a4f2b13bd8d90088153772fe5670b1
Mahendra <2021.mahendra.girase@ves.ac.in>
2023-11-25 19:39:01 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hare krishna");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please Enter some input:");
+        int num = input.nextInt();
+        System.out.println("Your num is "+num);
+    }
+}
